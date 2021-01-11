1985fdc5f257c16b5417edaed97c6a1231a06bf1
Nguyen, Victor <vdn140030@gmail.com>
2021-01-11 13:45:58 -0600

    refactor(App.js): set the state logic on the App level via State Hooks

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 function App() {
+  const [list, setList] = useState([]);
+  const [item, setItem] = useState('');
+
+  const addItem = e => {
+    e.preventDefault();
+    let newList = [...list];
+
+    newList.push(
+      {
+        item,
+        category: '',
+        quantity: '',
+        price: '',
+      }
+    );
+
+    setList(newList);
+    setItem('');
+  }
+
   return (<>
-    <AddItem />
+    <form onSubmit={addItem}>
+      <input value={item} onChange={e => setItem(e.target.value)} placeholder="Add a new item"/>
+      <button type="submit" disabled={!item}>Add</button>
+    </form>
   </>);
 }
 
 export default App;
-
-function AddItem() {
-  return (
-    <form>
-      <input />
-      <button>Add item</button>
-    </form>
-  );
-}
