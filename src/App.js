499761349d75d43ac81b21b6d1d7d15a6aa4b391
Nguyen, Victor <vdn140030@gmail.com>
2021-01-11 13:19:31 -0600

    feat(App.js): add a basic set up for the form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function App() {
-  return (
-    <div>
-      <header>
-        Hello World!
-      </header>
-      <input />
-    </div>
-  );
+  return (<>
+    <AddItem />
+  </>);
 }
 
 export default App;
+
+function AddItem() {
+  return (
+    <form>
+      <input />
+      <button>Add item</button>
+    </form>
+  );
+}
